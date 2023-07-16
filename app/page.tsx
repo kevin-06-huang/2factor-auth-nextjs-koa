c2f4e2ac69adb24a65d7e1624aa7108669ee78d6
Kevin <kevin.06.huang@gmail.com>
2023-07-16 16:37:40 -0700

    Add Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-'use client'
+import Navbar from "@/component/Navbar"
 
 export default function Home() {
   return (
     <>
+      <header className='flex flex-col justify-between p-6'>
+        <Navbar />
+      </header>
       <section className="bg-ct-blue-600 min-h-screen">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
           <p className="text-3xl font-semibold">
--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,6 +1,9 @@
+'use client'
+import { useState } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [test, setTest] = useState();
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
