0c44798d656f76149da2699f02236039c2de9963
Kevin <kevin.06.huang@gmail.com>
2023-07-17 09:20:36 -0700

    Add login frontend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
       <section className="bg-ct-blue-600 min-h-screen">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <p className="text-3xl font-semibold">
-            Login brah
-          </p>
+          <form className="font-semibold" action="/send-data-here" method="post">
+            <div className="ml-9">
+              <label htmlFor="email">Email:</label>
+              <input type="text" id="email" name="email" className="ml-2"/>
+            </div>
+            <div className="flex items-center mt-2">
+              <label htmlFor="password" className="block">Password:</label>
+              <input type="text" id="password" name="password" className="ml-2"/>
+            </div>
+            <div className="flex items-center mt-2 ml-12">
+              <button type="submit" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Login</button>
+              <Link href="/register" className="ml-2 font-light block">I&apos;m not a member</Link>
+            </div>
+          </form>
         </div>
       </section>
     </>
