fb44bb39a5bfb6d4ce014eaa3e5837ba544ab914
Kevin <kevin.06.huang@gmail.com>
2023-07-17 15:03:09 -0700

    Add logic for 2fa

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 export default function Profile() {
   const store = useStore()
   const router = useRouter()
+  const user = store.authUser
 
   useEffect(() => {
     if (!store.authUser) {
@@ -15,12 +16,19 @@ export default function Profile() {
 
   return (
     <>
-      {store.authUser ?
+      {user ?
         <section className="bg-ct-blue-600 min-h-screen">
           <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
             <p className="text-3xl font-semibold">
-              Profile, dude
+              Profile of {user.email}!!!
+              {
+                !user.otp_enabled ?
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded block mt-4 ml-24">Enable 2fa</button> :
+                <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded block mt-4 ml-24">Disable 2fa</button>
+              }
+              
             </p>
+            <></>
           </div>
         </section> :
         <section className="bg-ct-blue-600 min-h-screen">
