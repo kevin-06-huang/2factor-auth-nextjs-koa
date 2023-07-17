7e183b8fa84df37dc1e881bc94611e8a910b510e
Kevin <kevin.06.huang@gmail.com>
2023-07-16 20:31:23 -0700

    Add router

--- authServer/server.ts
+++ authServer/server.ts
@@ -1,10 +1,13 @@
-import koa from "koa"
-import router from "koa-router"
+import Koa from "koa"
+import Router from "koa-router"
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 3000
 
-const app = new koa()
+const app = new Koa()
+const router = new Router()
+
+app.use(router.routes())
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
