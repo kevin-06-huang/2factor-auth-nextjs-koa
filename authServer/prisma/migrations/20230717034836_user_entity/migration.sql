415b1d2a0eaf45d33b0329a89485d625c42dfafc
Kevin <kevin.06.huang@gmail.com>
2023-07-16 20:50:28 -0700

    sqlite DB up and running; trying postgresql

---
+++ authServer/prisma/migrations/20230717034836_user_entity/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "otp_enabled" BOOLEAN NOT NULL DEFAULT false,
+    "otp_verified" BOOLEAN NOT NULL DEFAULT false,
+    "otp_ascii" TEXT,
+    "otp_hex" TEXT,
+    "otp_base32" TEXT,
+    "otp_auth_url" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
