3948d081b3ea804564fb5ed4e9205bbb012968ed
Kevin <kevin.06.huang@gmail.com>
2023-07-17 09:28:31 -0700

    Change input styling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <>
       <section className="bg-ct-blue-600 min-h-screen">
         <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <form className="font-semibold" action="/send-data-here" method="post">
+          <form className="font-semibold" action="/register" method="post">
             <div className="ml-9">
               <label htmlFor="email">Email:</label>
-              <input type="text" id="email" name="email" className="ml-2"/>
+              <input type="text" id="email" name="email" className="ml-2 text-black"/>
             </div>
             <div className="flex items-center mt-2">
               <label htmlFor="password" className="block">Password:</label>
-              <input type="text" id="password" name="password" className="ml-2"/>
+              <input type="text" id="password" name="password" className="ml-2 text-black"/>
             </div>
             <div className="flex items-center mt-2 ml-12">
               <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Register</button>
