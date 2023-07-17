e5cbda2dee8bf5f489e074547f566f44a2054fdf
Kevin <kevin.06.huang@gmail.com>
2023-07-16 21:27:08 -0700

    Database setup, building koa

--- authServer/server.ts
+++ authServer/server.ts
@@ -2,13 +2,28 @@ import Koa from "koa"
 import Router from "koa-router"
 import 'dotenv/config'
 
+import { PrismaClient } from "@prisma/client"
+export const prisma = new PrismaClient()
+
 const PORT = process.env.PORT || 3000
 
-const app = new Koa()
-const router = new Router()
+async function main() {
+  const app = new Koa()
+  const router = new Router()
 
-app.use(router.routes())
+  app.use(router.routes())
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-})
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
