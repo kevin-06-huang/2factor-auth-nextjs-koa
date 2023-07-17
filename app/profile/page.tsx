07507102f6cb1d107ac1ca59796b321df0b2a670
Kevin <kevin.06.huang@gmail.com>
2023-07-17 16:29:08 -0700

    GenerateOTP complete on server

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,6 +8,18 @@ export default function Profile() {
   const router = useRouter()
   const user = store.authUser
 
+  const generateQrCode = async () => {
+    fetch('http://localhost:3000/otp/generate', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: user!.email
+      })
+    })
+  }
+
   useEffect(() => {
     if (!store.authUser) {
       router.push("/login");
@@ -23,7 +35,9 @@ export default function Profile() {
               Profile of {user.email}!!!
               {
                 !user.otp_enabled ?
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded block mt-4 ml-24">Enable 2fa</button> :
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded block mt-4 ml-24" onClick={generateQrCode}>
+                  Enable 2fa
+                </button> :
                 <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded block mt-4 ml-24">Disable 2fa</button>
               }
               
--- authServer/controllers/authController.ts
+++ authServer/controllers/authController.ts
@@ -1,5 +1,8 @@
 import Koa from "koa"
 import { prisma } from "../server";
+import crypto from "crypto";
+import { encode } from "hi-base32";
+import OTPAuth from "otpauth";
 
 const RegisterUser = async (ctx: Koa.Context) => {
   try {
@@ -44,8 +47,52 @@ const LoginUser = async (ctx: Koa.Context) => {
   }
 }
 
+const generateRandomBase32 = () => {
+  const buffer = crypto.randomBytes(15);
+  const base32 = encode(buffer).replace(/=/g, "").substring(0, 24);
+  return base32;
+};
+
 const GenerateOTP = async (ctx: Koa.Context) => {
-  console.log(ctx)
+  try {
+    const { email } = ctx.request.body as any
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if(!user) {
+      ctx.status = 404
+      ctx.body = { status: 'fail to find user' }
+    } else {
+      const base32Secret = generateRandomBase32()
+      
+      const totp = new OTPAuth.TOTP({
+        issuer: "Kevin Huang",
+        label: "2FA Example",
+        algorithm: "SHA1",
+        digits: 6,
+        period: 15,
+        secret: base32Secret,
+      });
+
+      const otpAuthUrl = totp.toString()
+
+      await prisma.user.update({
+        where: { email },
+        data: {
+          otp_auth_url: otpAuthUrl,
+          otp_base32: base32Secret,
+        },
+      })
+      
+      ctx.status = 200
+      ctx.body = {
+        base32Secret,
+        otpAuthUrl
+      }
+    }
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = 'error'
+  }
 }
 
 const VerifyOTP = async (ctx: Koa.Context) => {
