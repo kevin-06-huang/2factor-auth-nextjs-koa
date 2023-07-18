c2c8e4228943763e1c9119997d9949944d75b677
Kevin <kevin.06.huang@gmail.com>
2023-07-17 21:32:24 -0700

    Fix the logout button on validate

--- app/validateOtp/page.tsx
+++ app/validateOtp/page.tsx
@@ -19,7 +19,7 @@ export default function ValidateOtp() {
   return (
     <>
       {user ? (
-        <div className="fixed top-0 right-0 left-0 z-50 w-full md:inset-0 md:h-full bg-[#222] bg-opacity-50">
+        <div className="fixed top-0 right-0 left-0 z-[-1] w-full md:inset-0 md:h-full bg-[#222] bg-opacity-50">
           <div className="relative top-36 p-4 w-full max-w-xl h-full md:h-auto left-1/2 -translate-x-1/2">
             <div className="relative bg-white rounded-lg shadow">
               <h4 className="text-black flex justify-center font-semibold relative top-2 mb-4">
