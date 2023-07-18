50d168bcbae684683a40fa8c09a57e733360d849
Kevin <kevin.06.huang@gmail.com>
2023-07-17 18:09:43 -0700

    Add qr code and library

--- components/TwoFactorModal.tsx
+++ components/TwoFactorModal.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import QRCode from 'qrcode'
 
 type TwoFactorModalProps = {
   email: string;
@@ -11,16 +12,27 @@ const TwoFactorModal: FC<TwoFactorModalProps> = ({
   otpAuthUrl,
   closeModal
 }) => {
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
+
+  useEffect(() => {
+    QRCode.toDataURL(otpAuthUrl).then(setQrCodeUrl);
+  }, []);
+
   return (
     <div
       className="fixed top-0 right-0 left-0 z-50 w-full md:inset-0 md:h-full bg-[#222] bg-opacity-50"
     >
       <div className="relative top-36 p-4 w-full max-w-xl h-full md:h-auto left-1/2 -translate-x-1/2">
         <div className="relative bg-white rounded-lg shadow">
-          <h3 className="text-black">Two-Factor Authentication (2FA)</h3>
             <div>
-              <h4 className="text-black">Scan QR Code</h4>
-              <h4 className="text-black">{otpAuthUrl}</h4>
+              <h4 className="text-black flex justify-center font-semibold relative top-2">Scan QR Code</h4>
+              <div className="flex justify-center">
+                <img
+                  className="block w-64 h-64 object-contain"
+                  src={qrCodeUrl}
+                  alt="qrcode url"
+                />
+              </div>
             </div>
         </div>
       </div>
