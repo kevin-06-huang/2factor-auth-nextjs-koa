44eea8ba92bb3fb9d1a536adaeba5be30bd8c832
Kevin <kevin.06.huang@gmail.com>
2023-07-18 00:09:23 -0700

    Fix Event typing

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { Event } from "../../types";
 import { useRef } from "react";
 
 export default function Register() {
